40c75be440becff3d170889a82636e2d9c708583
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2023-06-19 23:17:24 +0800

    first commit

---
+++ exercise2.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+    alert("Ini video cupid");
+});
+
+audio.addEventListener("click", function audio() {
+    alert("ini audio cupid");
+})
+
+gambar.addEventListener("click", function gambar() {
+    alert("ini gambar cupid");
+});
